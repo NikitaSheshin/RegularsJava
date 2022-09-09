dfc48fe2d20fe15977cad17958a1c9f19dd6eaaa
Nikita Shehsin <sheshin.nikita21@yandex.ru>
2022-09-09 22:58:13 +0300

    Сделал первое задание

--- Regulars.java
+++ Regulars.java
@@ -1,3 +1,9 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Regulars {
-    public Boolean checkIP(String ip) { return false; }
+    public Boolean checkIP(String ip) {
+        return ip.matches("^([01]?\\d\\d?|25[0-5]|2[0-4]\\d)\\." +
+            "([01]?\\d\\d?|25[0-5]|2[0-4]\\d)\\.([01]?\\d\\d?|25[0-5]|2[0-4]\\d)\\.([01]?\\d\\d?|25[0-5]|2[0-4]\\d)$");
+    }
 }
--- RegularsTest.java
+++ RegularsTest.java
@@ -1,16 +1,13 @@
 import org.junit.*;
 
-import static org.junit.Assert.*;
-
-
 public class RegularsTest {
-    Regulars reg;
+    static Regulars reg;
 
     @BeforeClass
-    public void init() { reg = new Regulars(); }
+    public static void init() { reg = new Regulars(); }
 
     @AfterClass
-    public void dispose() { reg = null; }
+    public static void dispose() { reg = null; }
 
     @Test
     public void checkTrueIP(){
@@ -21,8 +18,8 @@ public class RegularsTest {
 
     @Test
     public void checkFalseIP(){
-        Assert.assertTrue(reg.checkIP("0,0.0.255"));
-        Assert.assertTrue(reg.checkIP("255.dgg.255.0"));
-        Assert.assertTrue(reg.checkIP("34.54.1347.234"));
+        Assert.assertFalse(reg.checkIP("0,0.0.255"));
+        Assert.assertFalse(reg.checkIP("255.dgg.255.0"));
+        Assert.assertFalse(reg.checkIP("34.54.1347.234"));
     }
 }
