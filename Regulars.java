863ff61d28751e7d4cee2f2ab2a5afd0a13145af
Nikita Shehsin <sheshin.nikita21@yandex.ru>
2022-09-10 13:44:16 +0300

    Сделал 4 задание

--- Regulars.java
+++ Regulars.java
@@ -7,12 +7,16 @@ public class Regulars {
             "([01]?\\d\\d?|25[0-5]|2[0-4]\\d)\\.([01]?\\d\\d?|25[0-5]|2[0-4]\\d)\\.([01]?\\d\\d?|25[0-5]|2[0-4]\\d)$");
     }
 
-    public Boolean checkGUID(String ip){
-        return ip.matches("^[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}$");
+    public Boolean checkGUID(String guid){
+        return guid.matches("^[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}$");
     }
 
-    public Boolean checkURL(String ip){
-        return ip.matches
+    public Boolean checkURL(String url){
+        return url.matches
                 ("^((http|https)?:\\/\\/(www\\.)?)?[0-9A-Za-z+~s\\/#=\\+?@%\\.][0-9A-Za-z+~s\\/\\-\\+?#=\\\\@%\\.]{0,254}[0-9A-Za-z+~s\\/\\+?#=\\\\@%\\.]\\.[a-z]{2,6}$");
+    }
+
+    public Boolean checkPass(String pass){
+        return pass.matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])[\\w]{8,}$");
     }
 }
