0e28a06c578e76bf77291c1ba8a540824f0a4093
Nikita Shehsin <sheshin.nikita21@yandex.ru>
2022-09-10 10:51:08 +0300

    Создал тесты для второго задания

--- RegularsTest.java
+++ RegularsTest.java
@@ -22,4 +22,18 @@ public class RegularsTest {
         Assert.assertFalse(reg.checkIP("255.dgg.255.0"));
         Assert.assertFalse(reg.checkIP("34.54.1347.234"));
     }
+
+    @Test
+    public void checkTrueGUID(){
+        Assert.assertTrue(reg.checkGUID("e02fd0e4-00fd-090A-ca30-0d00a0038ba0"));
+        Assert.assertTrue(reg.checkGUID("f0f0b61c-3c47-486c-ae5d-09dcbdafb921"));
+        Assert.assertTrue(reg.checkGUID("07E5C308-8E9D-48F4-9E51-C801A00A521A"));
+    }
+
+    @Test
+    public void checkFalseGUID(){
+        Assert.assertFalse(reg.checkGUID("e02fd0e400fd090Aca300d00a0038ba0"));
+        Assert.assertFalse(reg.checkGUID("f0f0b1c-3c47-486c-a5d-09dcbdafb91"));
+        Assert.assertFalse(reg.checkGUID("e02fd.e4-00fd-09A-ca30-0d00a0?38ba0"));
+    }
 }
